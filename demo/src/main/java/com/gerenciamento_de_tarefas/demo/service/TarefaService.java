193fa58d5a169a6ac52fc6db40488abad02fb572
BrundoCJ <bconrado04@gmail.com>
2026-03-24 17:28:39 -0300

    adicionnado service

--- demo/src/main/java/com/gerenciamento_de_tarefas/demo/service/TarefaService.java
+++ demo/src/main/java/com/gerenciamento_de_tarefas/demo/service/TarefaService.java
@@ -1,4 +1,38 @@
 package com.gerenciamento_de_tarefas.demo.service;
 
+import com.gerenciamento_de_tarefas.demo.entity.Tarefa;
+import com.gerenciamento_de_tarefas.demo.repository.TarefaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TarefaService {
+
+    private final TarefaRepository tarefaRepository;
+
+    public TarefaService(TarefaRepository tarefaRepository) {
+        this.tarefaRepository = tarefaRepository;
+    }
+
+    public Tarefa salvar(Tarefa tarefa) {
+        return tarefaRepository.save(tarefa);
+    }
+
+    public List<Tarefa> listarTodos() {
+        return tarefaRepository.findAll();
+    }
+
+    public Optional<Tarefa> buscarPorId(Long id) {
+        return tarefaRepository.findById(id);
+    }
+
+    public boolean deletar(Long id) {
+        if (tarefaRepository.existsById(id)) {
+            tarefaRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
