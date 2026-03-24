7d28d785983bf2bd31cc5d3805dc7f2bec2177c3
BrundoCJ <bconrado04@gmail.com>
2026-03-24 17:28:04 -0300

    adicionando repository

--- demo/src/main/java/com/gerenciamento_de_tarefas/demo/repository/TarefaRepository.java
+++ demo/src/main/java/com/gerenciamento_de_tarefas/demo/repository/TarefaRepository.java
@@ -1,4 +1,9 @@
 package com.gerenciamento_de_tarefas.demo.repository;
 
-public class TarefaRepository {
+import com.gerenciamento_de_tarefas.demo.entity.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 }
